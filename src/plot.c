66e03e2180cc28bf57a6659d9c6c2db7ab10a829
Christos Papaioannou <christos@christos-MacBook.(none)>
2013-11-20 18:36:07 +0200

    Added plot module.BUGS EVERYWHERE

--- src/plot.c
+++ src/plot.c
@@ -1,4 +1,27 @@
+#include <stdio.h>
+#include <string.h>
 #include "plot.h"
+
+
+#define MAX_STRINGS 100
+
+char *node_names[MAX_STRINGS];
+int num_node_names;
+
+
+/*
+ * Call this before using the plot methods
+ */
+void plot_init(){
+	int i;
+
+	num_node_names = 0 ; 
+
+	for( i = 0 ; i < MAX_STRINGS ; i++ )
+		node_names[i] = NULL;
+
+}
+
 
 /*
  * Return the size  of an array of vectors
@@ -15,10 +38,29 @@ int plot_find_size( double startv , double endv , double inc ){
 }
 
 /*
+ * Adds a node name for plotting
+ * Returns 1 when ok
+ *         0 when failed
+ */
+int plot_add_node(char* node_name){
+
+	if( num_node_names > MAX_STRINGS )
+		return 0;
+
+	node_names[num_node_names] = strdup(node_name);
+	if( !node_names[num_node_names] )
+		return 0;
+
+	num_node_names++;
+	return 1;
+
+}
+
+/*
  * Gets an array of gsl_vectors.Starting values = 0.0
  * returns : NULL at failure
  */
-gsl_vector** plot_init_vector( int array_size  ,  int vector_size ){
+gsl_vector** plot_create_vector( int array_size  ,  int vector_size ){
 
 	int i;
 	gsl_vector **vector_array = NULL;
@@ -39,4 +81,58 @@ gsl_vector** plot_init_vector( int array_size  ,  int vector_size ){
 	}
 
 	return vector_array;
+}
+
+/*
+ * writes an array of vectors to a file 
+ */
+void plot_to_file( hashtable_t* hashtable, gsl_vector** array , int array_size, char* filename){
+
+	int i,j;
+	//int vector_size;
+	int node_id;
+
+	FILE* file;
+	file = fopen( filename , "w");
+	if( !file ){
+		printf("Plot Failed\n");
+		return;
+	}
+
+	fprintf(file, "##### SOLUTIONS ######\n");
+
+	for( i = 0 ; i < array_size ; i++ ){
+		fprintf(file, "Solution: %d\n",(i+1) );
+		//vector_size = array[i]->size;
+				
+		for( j = 0 ; j < num_node_names ; j++){
+			ht_get(hashtable , node_names[j] , &node_id);
+			fprintf(file, "\t%s : %f\n", node_names[j] , gsl_vector_get( array[i] , node_id - 1 ) );
+
+		}
+		fprintf(file, "\n");
+	}
+}
+
+/*
+ * Copies vector b at array[index]
+ */
+int plot_set_vector_index(gsl_vector** array , gsl_vector* b , int index){
+	return gsl_vector_memcpy( array[index] , b);
+
+}
+
+
+/*
+ * Deallocates all vectors
+ */
+void plot_free_array(gsl_vector** array , int array_size){
+
+	int i;
+	for( i = 0 ; i < array_size ; i ++ ){
+		gsl_vector_free(array[i]);
+	}
+
+	free( array);
+
 }
